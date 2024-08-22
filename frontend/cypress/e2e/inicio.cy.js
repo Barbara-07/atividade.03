3f78c662e74ab871aa3bc21dffdd4fc26db00683
barbaracarvsouza07@gmail.com <barbaracarv.souza07@gmail.com>
2024-08-22 14:41:10 -0300

    Criação de comandos personalizados

--- frontend/cypress/e2e/inicio.cy.js
+++ frontend/cypress/e2e/inicio.cy.js
@@ -1,5 +1,13 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Pagina inicial', () => {
+  beforeEach( () => {
+    it('passes', () => {
+      cy.visit('https://localhost:3000')
+    })
+    it('Deve renderizar h1 com o texto correto!', () =>{
+  
+      cy.agetByData('titulo-principal').containts("Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente")
+  
+    })
   })
 })
+
--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('containtsText', (selector, text) =>{
+    return cy.get(`[data-test=${selector}]`).containts(text)
+})
+
+// Cypress.Command.add('getByData', (seletor) => {
+//     return cy.get(`[data-test=${seletor}]`)
+// })
