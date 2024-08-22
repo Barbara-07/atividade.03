f67527efec8159bce230e41766f2370fc9d47d1b
barbaracarvsouza07@gmail.com <barbaracarv.souza07@gmail.com>
2024-08-22 14:53:07 -0300

    Criação de um comando que recebe dois parâmetros

--- frontend/cypress/e2e/inicio.cy.js
+++ frontend/cypress/e2e/inicio.cy.js
@@ -5,7 +5,7 @@ describe('Pagina inicial', () => {
     })
     it('Deve renderizar h1 com o texto correto!', () =>{
   
-      cy.agetByData('titulo-principal').containts("Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente")
+      cy.agetByData('titulo-principal', 'Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente')
   
     })
   })
