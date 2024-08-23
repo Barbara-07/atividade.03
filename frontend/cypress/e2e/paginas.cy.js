afed41652143908e9e695d4b44fb32af3a65a710
barbaracarvsouza07@gmail.com <barbaracarv.souza07@gmail.com>
2024-08-23 10:59:42 -0300

    Validação de serviço

--- frontend/cypress/e2e/paginas.cy.js
+++ frontend/cypress/e2e/paginas.cy.js
@@ -9,6 +9,16 @@ describe('Testando múltiplas páginas', () => {
     cy.getByData('titulo-cartoes').should('exist').and('have.text','Meus cartões')
   })
 
+  it('Deve conseguir acessar a página de serviços', () => {
+    cy.visit('/')
+    cy.getByData('botao-login').click()
+    cy.getByData('email-input').type('neilton@alura.com')
+    cy.getByData('senha-input').type('123456')
+    cy.getByData('botao-enviar').click()
+    cy.getByData('app-home').find('a').eq(1).click()
+    cy.getByData('app-home').find('h5').contains('Pix')
+  })
+
 })
 
 
